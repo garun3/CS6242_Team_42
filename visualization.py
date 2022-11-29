8b0f6fa1e79a5600bdf2ff87d0af8e470d5b3500
ryantobin77 <ryantobin77@gmail.com>
2022-11-29 17:51:02 -0500

    Finished first draft of visualization

--- visualization.py
+++ visualization.py
@@ -9,75 +9,54 @@ feature_map = dict()
 feature_map["Happiness Rank"] = "Happiness_Score_Percentile"
 
 app.layout = html.Div([
-    html.H4('World happiness visualization'),
-    html.P("Select a feature to visualize:"),
-    dcc.RadioItems(
-        id='feature', 
-        options=["Happiness Rank", "Social Support", "Log GDP per capita", "Life Expectancy", "Freedome to make life choices"],
-        value="Happiness Rank",
-        inline=True
-    ),
+    html.H3('World happiness visualization', style={'marginTop': '10', 'marginBottom': '0'}),
+    html.Div([
+        html.Div([
+            html.P("Select a feature to visualize:"),
+            dcc.Dropdown(["Happiness Rank", "Social Support", "Log GDP per capita", "Life Expectancy", "Freedom to make life choices"], 'Happiness Rank', id='feature'),
+        ], style={"width": "30%", 'float': 'left'}),
+        html.Div([
+            html.P("Select the year to visualize:"),
+            dcc.Slider(13, 21, step=None,
+                marks={
+                13: '2013',
+                14: '2014',
+                15: '2015',
+                16: '2016',
+                17: '2017',
+                18: '2018',
+                19: '2019',
+                20: '2020',
+                21: '2021',
+                }, value=13, id='year'),
+        ], style={"width": "65%", 'float': 'right'}),
+    ], style={'width': '100%', 'height' : '100px'}),
     dcc.Graph(id="graph"),
-])
+], style={"width": "100%", "height" : "100%"})
 
 @app.callback(
     Output("graph", "figure"), 
-    Input("feature", "value")
-    )
+    Input("feature", "value"),
+    Input("year", "value")
+)
 
-def display_choropleth(feature):
-    # df = px.data.election() # replace with your own data source
-    # geojson = px.data.election_geojson()
-    # fig = px.choropleth(
-    #     df, geojson=geojson, color=candidate,
-    #     locations="district", featureidkey="properties.district",
-    #     projection="mercator", range_color=[0, 6500])
-    # fig.update_geos(fitbounds="locations", visible=False)
-
-
-    # df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
-    # with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    #    counties = json.load(response)
-    # geojson = counties
-    # fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    # fig = px.choropleth(df, geojson=geojson, locations='fips', color='unemp',
-    #                        color_continuous_scale="Viridis",
-    #                        range_color=(0, 12),
-    #                        scope="usa",
-    #                        labels={'unemp':'unemployment rate'}
-    #                       )
-    # fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-
-    feature_val = "Happiness_Score_Percentile" # feature_map[feature]
+def display_choropleth(feature, year):
+    year = 2000 + int(year)
     df = pd.read_csv("normalized_data.csv")
-    json_file = open("world_countries.json")
-    geojson = json.load(json_file)
-    json_file.close()
-    fig = px.choropleth(df, geojson=geojson, locations='Country', color=feature_val,
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 9),
-                           scope='world',
-                           labels={'Happiness_Score_Percentile': 'Happiness Rank'}
-                          )
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    df = df[df['Year'] == year]
+    fig = px.choropleth(df, locations="Country",
+                    color="Happiness_Score_Percentile", 
+                    hover_name="Country",
+                    hover_data=['Happiness_Score_Percentile','Social support', 'Log GDP per capita', 'Healthy life expectancy at birth', 'Freedom to make life choices'], 
+                    color_continuous_scale='Greens',
+                    locationmode='country names',
+                    range_color=[0, 9],
+                    projection='natural earth',
+                    scope ='world') 
+    fig.update_layout(margin={"r" : 0, "t" : 10, "l" : 0, "b" : 0}, 
+                        autosize=False, width=1300, height=600)
+    fig.update_coloraxes(colorbar_len=0.75)
     return fig
 
 
-app.run_server(debug=True)
-
-# with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-#     counties = json.load(response)
-
-# # counties["features"][0]
-# df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
-#                    dtype={"fips": str})
-# # df.head()
-
-# fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
-#                            color_continuous_scale="Viridis",
-#                            range_color=(0, 12),
-#                            scope="usa",
-#                            labels={'unemp':'unemployment rate'}
-#                           )
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+app.run_server(debug=False)
