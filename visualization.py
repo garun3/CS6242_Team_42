df2f477b743596b452cb3ac6194c431c83cd1d2c
garun3 <gautham.arun2000@gmail.com>
2022-11-29 18:59:12 -0500

    second draft of visualization

--- visualization.py
+++ visualization.py
@@ -6,10 +6,15 @@ from dash import Dash, dcc, html, Input, Output
 
 app = Dash(__name__)
 feature_map = dict()
-feature_map["Happiness Rank"] = "Happiness_Score_Percentile"
+feature_map = {"Happiness Rank": "Happiness_Score_Percentile",
+                'Social Support':'Social support',
+                'Log GDP per capita': 'Log GDP per capita',
+                'Life Expectancy': 'Healthy life expectancy at birth',
+                'Freedom to make life choices': 'Freedom to make life choices'
+}
 
 app.layout = html.Div([
-    html.H3('World happiness visualization', style={'marginTop': '10', 'marginBottom': '0'}),
+    html.H3('World Happiness Visualization', style={'marginTop': '10', 'marginBottom': '0'}),
     html.Div([
         html.Div([
             html.P("Select a feature to visualize:"),
@@ -32,7 +37,19 @@ app.layout = html.Div([
         ], style={"width": "65%", 'float': 'right'}),
     ], style={'width': '100%', 'height' : '100px'}),
     dcc.Graph(id="graph"),
-], style={"width": "100%", "height" : "100%"})
+    html.H4('Index:'),
+    html.P('Happiness Rank: Measured from 1 to 10, with 10 meaning a country is in the top 10% of countries for happiness'),
+    
+    html.P('Social Support: Average of binary responses to the question "If you were in trouble, do you have relatives or friends you can count on to help you whenever you need them, or not?"'),
+
+    html.P('Log GDP per capita: GDP per capita on a log scale'),
+
+    html.P('Life Expectancy: Displayed in years'),
+
+    html.P('Freedom to make life choices: Average of binary responses to the question "Are you satisfied or dissatisfied with your freedom to choose what you do with your life?"')
+], 
+style={"width": "100%", "height" : "100%"}
+    )
 
 @app.callback(
     Output("graph", "figure"), 
@@ -42,19 +59,32 @@ app.layout = html.Div([
 
 def display_choropleth(feature, year):
     year = 2000 + int(year)
-    df = pd.read_csv("normalized_data.csv")
+    df = pd.read_csv("all_data.csv")
     df = df[df['Year'] == year]
+    df['Happiness_Score_Percentile'] = abs(df['Happiness_Score_Percentile'] - 10)
+    #print(feature)
+    feat = feature_map[feature]
     fig = px.choropleth(df, locations="Country",
-                    color="Happiness_Score_Percentile", 
+                    color=feat, 
                     hover_name="Country",
                     hover_data=['Happiness_Score_Percentile','Social support', 'Log GDP per capita', 'Healthy life expectancy at birth', 'Freedom to make life choices'], 
-                    color_continuous_scale='Greens',
+                    color_continuous_scale='RdYlGn',
                     locationmode='country names',
-                    range_color=[0, 9],
+                    #range_color=[1, 10],
                     projection='natural earth',
                     scope ='world') 
     fig.update_layout(margin={"r" : 0, "t" : 10, "l" : 0, "b" : 0}, 
-                        autosize=False, width=1300, height=600)
+                        autosize=False, width=1300, height=500,
+                        coloraxis_colorbar=dict(
+                        title=feature,
+                        orientation='h'
+                        #thicknessmode="pixels",
+                        #lenmode="pixels",
+                        #yanchor="top",y=1,
+                        #ticks="outside",
+                        #tickvals=[1,2,3,4,5,6,7,8,9,10],#[10,9,8,7,6,5,4,3,2,1],
+                        #ticktext=[1,2,3,4,5,6,7,8,9,10]#[10,9,8,7,6,5,4,3,2,1]
+))
     fig.update_coloraxes(colorbar_len=0.75)
     return fig
 
