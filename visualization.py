4a75c47b0c6100dabb5416759a4e96ce006bc217
ryantobin77 <ryantobin77@gmail.com>
2022-11-30 00:35:15 -0500

    UI improvements to visualization

--- visualization.py
+++ visualization.py
@@ -5,13 +5,9 @@ import plotly.express as px
 from dash import Dash, dcc, html, Input, Output
 
 app = Dash(__name__)
-feature_map = dict()
-feature_map = {"Happiness Rank": "Happiness_Score_Percentile",
-                'Social Support':'Social support',
-                'Log GDP per capita': 'Log GDP per capita',
-                'Life Expectancy': 'Healthy life expectancy at birth',
-                'Freedom to make life choices': 'Freedom to make life choices'
-}
+feature_map = {"Happiness_Score_Percentile" : "Happiness Rank",
+                'Social support' : 'Social Support',
+                'Healthy life expectancy at birth' : 'Life Expectancy',}
 
 app.layout = html.Div([
     html.H3('World Happiness Visualization', style={'marginTop': '10', 'marginBottom': '0'}),
@@ -39,17 +35,11 @@ app.layout = html.Div([
     dcc.Graph(id="graph"),
     html.H4('Index:'),
     html.P('Happiness Rank: Measured from 1 to 10, with 10 meaning a country is in the top 10% of countries for happiness'),
-    
     html.P('Social Support: Average of binary responses to the question "If you were in trouble, do you have relatives or friends you can count on to help you whenever you need them, or not?"'),
-
     html.P('Log GDP per capita: GDP per capita on a log scale'),
-
     html.P('Life Expectancy: Displayed in years'),
-
     html.P('Freedom to make life choices: Average of binary responses to the question "Are you satisfied or dissatisfied with your freedom to choose what you do with your life?"')
-], 
-style={"width": "100%", "height" : "100%"}
-    )
+], style={"width": "100%", "height" : "100%"})
 
 @app.callback(
     Output("graph", "figure"), 
@@ -62,15 +52,15 @@ def display_choropleth(feature, year):
     df = pd.read_csv("all_data.csv")
     df = df[df['Year'] == year]
     df['Happiness_Score_Percentile'] = abs(df['Happiness_Score_Percentile'] - 10)
-    #print(feature)
-    feat = feature_map[feature]
+    df = df.drop(columns=['Life Expectancy'])
+    df = df.rename(columns=feature_map)
+    hover_data = {"Country" : False, "Happiness Rank" : True, "Social Support" : ":.3f", "Log GDP per capita" : ":.3f", "Life Expectancy" : ":.2f", "Freedom to make life choices" : ":.3f"}
     fig = px.choropleth(df, locations="Country",
-                    color=feat, 
+                    color=feature, 
                     hover_name="Country",
-                    hover_data=['Happiness_Score_Percentile','Social support', 'Log GDP per capita', 'Healthy life expectancy at birth', 'Freedom to make life choices'], 
+                    hover_data=hover_data,
                     color_continuous_scale='RdYlGn',
                     locationmode='country names',
-                    #range_color=[1, 10],
                     projection='natural earth',
                     scope ='world') 
     fig.update_layout(margin={"r" : 0, "t" : 10, "l" : 0, "b" : 0}, 
@@ -78,14 +68,10 @@ def display_choropleth(feature, year):
                         coloraxis_colorbar=dict(
                         title=feature,
                         orientation='h'
-                        #thicknessmode="pixels",
-                        #lenmode="pixels",
-                        #yanchor="top",y=1,
-                        #ticks="outside",
-                        #tickvals=[1,2,3,4,5,6,7,8,9,10],#[10,9,8,7,6,5,4,3,2,1],
-                        #ticktext=[1,2,3,4,5,6,7,8,9,10]#[10,9,8,7,6,5,4,3,2,1]
-))
-    fig.update_coloraxes(colorbar_len=0.75)
+                    ))
+    fig.update_coloraxes(colorbar_len=0.75, colorbar_thickness=10)
+    fig.update_traces(marker_line_width=0.5, marker_line_color='white')
+    fig.update_geos(visible=False, showcountries=True, countrycolor="lightgray")
     return fig
 
 
